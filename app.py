88f2320eefc86c00c2d7ddab97ebc45dad024185
ShoolCodeRepository <20245@wc.school.nz>
2024-04-09 11:09:00 +1200

    Third commit

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
-from flask import Flask
+import sqlite3
+from sqlite3 import Error
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
+DATABASE = "C:/Users/20245/OneDrive - Wellington College/13DTS/Coding projects/flaskProject2/Dictionary"
 
+
+def create_connection(db_file):
+    #This function creates a connection with the database
+    #which means that the database file is open
+    #so that cursor objects can be created and used on the open database
+    try:
+        connection = sqlite3.Connection(db_file)
+        return connection
+    except Error as e:
+        print(e)
+    return e
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def render_home():
+    return render_template("home_page.html")
+
+@app.route('/dictionary')
+def render_dicionary():
+    return render_template("dictionary_page.html")
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/login')
+def render_login():
+    return render_template("login_page.html")
+
+
+
+app.run(host='0.0.0.0', debug=True)
